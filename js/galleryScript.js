a63e42481e7f1429bf6b0b1a156264047d7af49a
MatteoTP06 <matteo.torquatoperillo@elev.ga.ntig.se>
2024-03-20 14:05:47 +0100

    20-03

--- js/galleryScript.js
+++ js/galleryScript.js
@@ -1,8 +1,10 @@
+
+
 const gallery = document.getElementById('gallery')
 const prevButton = document.getElementById('prevButton')
 const nextButton = document.getElementById('nextButton')
 
-const images = ['img/gallery_TLB.png', 'img/gallery_jelly.png', 'img/gallery_pax.png'];
+const images = [['img/gallery_TLB.png', "On the surface lay a frigid and empty wasteland."], ['img/gallery_jelly.png', "blabla jelly"], ['img/gallery_pax.png', "Meet Pax and Floresco, the first sinners"]];
 let currentIndex = 0;
 
 prevButton.addEventListener('click', () => {
@@ -22,9 +24,13 @@ else if(currentIndex > (images.length - 1)){
     currentIndex = (0)
 }
   const img = gallery.querySelector('.currentImage');
+  const imgCaption = gallery.querySelector('.currentCaption');
   img.classList.add('fadeOut');
+  imgCaption.classList.add('fadeOut');
   setTimeout(() => {
-    img.src = images[currentIndex]
+    img.src = images[currentIndex][0]
     img.classList.remove('fadeOut');
+    imgCaption.innerHTML = images[currentIndex][1]
+    imgCaption.classList.remove('fadeOut');
   }, 500);
 }
